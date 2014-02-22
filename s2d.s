96092e6aedd2e42ab925e38cd7d8a59494207d1c
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-02-22 01:07:38 -0800

    Completed print dense matrix

--- s2d.s
+++ s2d.s
@@ -5,5 +5,32 @@
 # Row r<y> {int row#, Elem *node, Row *nextrow}
 # Elem e<y><x> {int col#, int value, Elem *nextelem}
 sparse2dense:
-	### YOUR CODE HERE ###
+	# store the pointer to the first row in a variable
+	# add	$s0, $a0, $0
+	li	$s1, 4
+	
+row_loop:
+	beq	$a0, $0, row_exit	# if (a0 == 0) goto row_exit
+	lw	$t0, 0($a0)		# t0 = *a0 # Row number
+	lw	$s0, 4($a0)		# s0 = *(a0 + 4) # Pointer to elem
+elem_loop:
+	beq	$s0, $0, elem_exit	# if (s0 == 0) goto elem_exit
+	lw	$t1, 0($s0)		# t1 = *s0 # Elem number
+	lw	$t2, 4($s0)		# t2 = *(s0 + 4) # Value
+	lw	$s0, 8($s0)		# t0 = *(s0 + 8) # Pointer to next elem
+	# mult	$t1, $s1		# lo = t1 * s1 = t1 * 4
+	# mflo	$t1			# t1 = lo = t1 * 4
+	add	$t1, $t1, $t1		# t1 = t1 + t1
+	add	$t1, $t1, $t1		# t1 = t1 + t1
+	mult	$t0, $a2
+	mflo	$t3
+	add	$t3, $t3, $t1
+	add	$t3, $t3, $a1
+	sw	$t2, ($t3)
+	j	elem_loop
+elem_end:		
+	
+	
+row_end:
+	
 	jr	$ra
